0c60f753aab58ace649f211bf4a7b7942f328501
Santiago <santiacunia_10@hotmail.com>
2015-04-18 00:43:52 -0300

    N y K son constantes. Sacados parametros de dimension

--- fe.py
+++ fe.py
@@ -1,6 +1,10 @@
 import re
 
 
+#Constantes N y K: N es la cantidad de dimensiones, K la cantidad de NN a considerar.
+N = 550
+K = 5
+
 def clean(s):
     """
         Limpia el string s.
@@ -8,7 +12,7 @@ def clean(s):
     """
     return " ".join(re.findall(r'\w+', s,flags = re.UNICODE | re.LOCALE)).lower()
 
-def get_data_tsv(loc_dataset, dimensiones):
+def get_data_tsv(loc_dataset):
     #Devuelve de a 1 linea.
     for e, line in enumerate(open(loc_dataset,"rb")):
         if e > 0:
@@ -23,22 +27,23 @@ def get_data_tsv(loc_dataset, dimensiones):
             features = []
             if len(r) == 3: #train set
                 for word in r[2].split(): 
-                    posicion = hash(word)%dimensiones
+                    posicion = hash(word)%N
                     if posicion not in features:
                         features.append(posicion)
                 label = int(r[1])
             else: #test set
-                features = [(hash(f)%dimensiones) for f in r[1].split()]
+                features = [(hash(f)%N) for f in r[1].split()]
                 label = 1
             #Saque los bigram, no tenia sentido dejarlos, se busca eficiencia y pocas dimensiones.
             yield label, id, features
-
-#def distance(vector1, vector2):
- 
-
-def cargarMatriz(archivo, dimensiones):
+def distance(v, w, N):
+    """
+    Calcula distancia entre dos vectores V y W, vectores dispersos que representan a otros 
+    P y Q de dimension N.
+    """
+def cargarMatriz(archivo):
     m = []
-    for i, (label, id, features) in enumerate( get_data_tsv(archivo, N) ):
+    for i, (label, id, features) in enumerate( get_data_tsv(archivo) ):
         m.append((id, label, sorted(features)))
     return m
 
@@ -51,10 +56,8 @@ if __name__ == "__main__":
                 y sus filas son las palabras que se "activaron".
        -Tomo el archivo de pruebas y por cada review que tengo
     """
-    N = 550
-    K = 5
-    entrenamiento = cargarMatriz("labeledTrainData.tsv",N) # matriz con reviews de entrenamiento.
+    entrenamiento = cargarMatriz("labeledTrainData.tsv") # matriz con reviews de entrenamiento.
     print entrenamiento[0]
     print len(entrenamiento[0][2])
     # carga matriz con reviews para prueba. Label sobra, pero es mas facil que reescribir el metodo. Se reescribe label con el resultado buscado.
-    prueba = cargarMatriz("testData.tsv", N) 
+    prueba = cargarMatriz("testData.tsv") 
